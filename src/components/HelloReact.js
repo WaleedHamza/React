b3e6590dc05db51d80c51717b8f6d8d4a99b23d1
Waleed Hamza <hamzah.waleed@gmail.com>
2018-07-04 11:08:35 -0400

    +Wrapper

---
+++ src/components/HelloReact.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import FriendCard from "./FriendCard/FriendCard";
+import Wrapper from "./Wrapper/Wrapper";
+const HelloReact = () => (
+<Wrapper>
+<p>Hello React!</p>
+ {/* <FriendCard /> */}
+</Wrapper>
+);
+
+export default HelloReact;
