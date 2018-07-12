8604ed57c86f74d7611e4476140de8d609f800cd
Waleed Hamza <hamzah.waleed@gmail.com>
2018-07-12 15:55:45 -0400

    remove card works

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -1,28 +1,28 @@
 import React from "react";
-import "./FriendCrad.css";
+import "./FriendCard.css";
 
-const FriendCrad = () => (
+const FriendCard = props => (
   <div className="card">
     <div className="img-container">
-      <img
-        alt="SpongeBob"
-        src="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Spongebob-squarepants.svg/666px-Spongebob-squarepants.svg.png"
-      />
+      <img alt={props.name} src={props.image} />
     </div>
     <div className="content">
       <ul>
         <li>
-          <strong>Name:</strong> SpongeBob
+          <strong>Name:</strong> {props.name}
         </li>
         <li>
-          <strong>Occupation:</strong> Fry Cook
+          <strong>Occupation:</strong> {props.occupation}
         </li>
         <li>
-          <strong>Location:</strong> A Pinapple Under the Sea
+          <strong>Location:</strong> {props.location}
         </li>
       </ul>
     </div>
+    <span onClick={() => props.removeFriend(props.id)} className="remove">
+      𝘅
+    </span>
   </div>
 );
 
-export default FriendCrad;
+export default FriendCard;
